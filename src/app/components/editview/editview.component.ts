76d14df6ff8c6a5086a8775f46b6e03f94ea0dd4
Chris <christoph_glatthor@web.de>
2018-11-21 16:50:50 +0100

    added font awesome 5.5

--- src/app/components/editview/editview.component.ts
+++ src/app/components/editview/editview.component.ts
@@ -18,6 +18,13 @@ export class EditviewComponent implements OnInit {
   selectedNote = {} as Note;
   selectedFont;
   iFrameDocument;
+  selectedFontColor;
+  selectedBackgroundColor;
+  colorList = ['000000', '993300', '333300', '003300', '003366', '000066', '333399', '333333',
+    '660000', 'FF6633', '666633', '336633', '336666', '0066FF', '666699', '666666', 'CC3333',
+    'FF9933', '99CC33', '669966', '66CCCC', '3366FF', '663366', '999999', 'CC66FF', 'FFCC33',
+    'FFFF66', '99FF66', '99CCCC', '66CCFF', '993366', 'CCCCCC', 'FF99CC', 'FFCC99', 'FFFF99',
+    'CCffCC', 'CCFFff', '99CCFF', 'CC99FF', 'FFFFFF'];
 
   constructor(private taskService: TaskService) { }
 
@@ -49,19 +56,19 @@ export class EditviewComponent implements OnInit {
   }
 
   selectFont() {
-    const selectFont = <HTMLSelectElement> document.getElementById('selectFont');
+    const selectFont = <HTMLSelectElement>document.getElementById('selectFont');
     console.log(selectFont);
     this.execCommandWithArg('fontName', selectFont.value);
   }
 
-  selectHeadline(){
-    const selectHeadline = <HTMLSelectElement> document.getElementById('selectHeadline');
+  selectHeadline() {
+    const selectHeadline = <HTMLSelectElement>document.getElementById('selectHeadline');
     console.log(selectHeadline);
     this.execCommandWithArg('formatBlock', selectHeadline.value);
   }
 
-  selectFontsize(){
-    const selectFontsize = <HTMLSelectElement> document.getElementById('selectFontsize');
+  selectFontsize() {
+    const selectFontsize = <HTMLSelectElement>document.getElementById('selectFontsize');
     console.log(selectFontsize);
     this.execCommandWithArg('fontSize', selectFontsize.value);
   }
